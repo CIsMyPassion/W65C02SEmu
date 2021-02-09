5683df5ec01e01498906b66ba961309696953ec4
CIsMyPassion <sjno@gmx.de>
2021-02-09 19:28:49 +0100

    all pins added

--- emuW65C02S/src/cpuW65C02S.cpp
+++ emuW65C02S/src/cpuW65C02S.cpp
@@ -1,6 +1,8 @@
 #include"include/emuW65C02S.h"
 
-void W65C02S::CPU::setReset(State state)
+void W65C02S::CPU::setPHI2(const State& state)
 {
-	REST.state = state;
+	PHI2.state = state;
+	PHI2O.state = state;
+	PHI1O.state = state == State::HIGH ? State::LOW : State::HIGH;
 }
--- emuW65C02S/src/include/emuW65C02S.h
+++ emuW65C02S/src/include/emuW65C02S.h
@@ -30,9 +30,49 @@ private:
 		A10, A11, A12, A13,
 		A14, A15;
 	
-	//reset pin
-	Pin REST;
+	//bus enable
+	Pin BE;
+
+	//data bus
+	Pin D0, D1, D2, D3,
+		D4, D5, D6, D7;
+
+	//interrupt request
+	Pin IRQB;
+
+	//memory block
+	Pin MLB;
+
+	//non-maskable interrupt
+	Pin NMIB;
 	
+	//phase 2 in
+	Pin PHI2;
+
+	//phase 2 out
+	Pin PHI2O;
+
+	//phase 1 out
+	Pin PHI1O;
+
+	//read/write
+	Pin RWB;
+
+	//ready
+	Pin RDY;
+
+	//reset pin
+	Pin RESB;
+
+	//set overflow
+	Pin SOB;
+
+	//synchronize with opcode fetch
+	Pin SYNC;
+
+	//vector pull
+	Pin VPB;
+
 public:
 	//address bus
 	const State& getA0() const { return A0.state; }
@@ -51,6 +91,78 @@ public:
 	const State& getA13() const { return A13.state; }
 	const State& getA14() const { return A14.state; }
 	const State& getA15() const { return A15.state; }
+	
+	//bus enable
+	const State& getBE() const { return BE.state; }
 
-	void setReset(State);
+	void setBE(const State& state) { BE.state = state; }
+	
+	//data bus
+	const State& getD0() const { return D0.state; }
+	const State& getD1() const { return D1.state; }
+	const State& getD2() const { return D2.state; }
+	const State& getD3() const { return D3.state; }
+	const State& getD4() const { return D4.state; }
+	const State& getD5() const { return D5.state; }
+	const State& getD6() const { return D6.state; }
+	const State& getD7() const { return D7.state; }
+
+	void setD0(const State& state) { D0.state = state; }
+	void setD1(const State& state) { D1.state = state; }
+	void setD2(const State& state) { D2.state = state; }
+	void setD3(const State& state) { D3.state = state; }
+	void setD4(const State& state) { D4.state = state; }
+	void setD5(const State& state) { D5.state = state; }
+	void setD6(const State& state) { D6.state = state; }
+	void setD7(const State& state) { D7.state = state; }
+
+	//interrupt request
+	const State& getIRQB() const { return IRQB.state; }
+	
+	void setIRQB(const State& state) { IRQB.state = state; }
+
+	//memory lock
+	const State& getMLB() const { return MLB.state; }
+	
+	//non-maskable interrupt
+	const State& getNMIB() const { return NMIB.state; }
+
+	void setNMIB(const State& state) { NMIB.state = state; }
+
+	//phase 2 in
+	const State& getPHI2() const { return PHI2.state; }
+	
+	void setPHI2(const State&);
+	
+	//phase 2 out
+	const State& getPHI2O() const { return PHI2O.state; }
+	
+	//phase 1 out
+	const State& getPHI1O() const { return PHI1O.state; }
+
+	//read/write
+	const State& getRWB() const { return RWB.state; }
+	
+	//ready
+	const State& getRDY() const { return RDY.state; }
+
+	void setRDY(const State& state) { RDY.state = state; }
+
+	//reset
+	const State& getRESB() const { return RESB.state; }
+
+	void setRESB(const State& state) { RESB.state = state; }
+
+	//set overflow
+	const State& getSOB() const { return SOB.state; }
+
+	void setSOB(const State& state) { SOB.state = state; }
+
+	//synchronize with opcode fetch
+	const State& getSYNC() const { return SYNC.state; }
+
+	//vector pull
+	const State& getVPB() const { return VPB.state; }
+
+	void setVPB(const State& state) { VPB.state = state; }
 };
