f47f77155427fe14fb0d9a3b0e917b2fa9ac1299
CIsMyPassion <sjno@gmx.de>
2021-02-12 00:31:44 +0100

    small modifications and file restructuring

--- emuW65C02S/src/include/emuW65C02S.h
+++ emuW65C02S/src/include/emuW65C02S.h
@@ -308,11 +308,57 @@ namespace W65C02S
 	class CPU;
 	class ByteRegister;
 	class WordRegister;
+	class InstructionDecoder;
 }
 
 struct W65C02S::Pin
 {
 	State state;
+};
+
+class W65C02S::ByteRegister
+{
+private:
+	//internal data byte
+	Byte data;
+
+public:
+	//get data
+	const Byte& getByte() const { return data; }
+
+	//set data
+	void setByte(const Byte& byte) { data = byte; }
+};
+
+class W65C02S::WordRegister
+{
+private:
+	//internal data word
+	Word data;
+public:
+	//get data
+	const Word& getWord() const { return data; }
+
+	//set data
+	void setWord(const Word& word) { data = word; }
+
+	//get the high byte of the internal data
+	const Byte& getHighByte();
+
+	//set the high byte of the internal data
+	void setHighByte(const Byte& byte);
+
+	//get the low byte of the internal data
+	const Byte& getLowByte();
+
+	//set the low byte of the internal data
+	void setLowByte(const Byte& byte);
+};
+
+class W65C02S::InstructionDecoder
+{
+private:
+	Byte instruction;
 };
 
 class W65C02S::CPU
@@ -480,43 +526,4 @@ public:
 	const State& getVPB() const { return VPB.state; }
 
 	void setVPB(const State& state) { VPB.state = state; }
-};
-
-class W65C02S::ByteRegister
-{
-private:
-	//internal data byte
-	Byte data;
-
-public:
-	//get data
-	const Byte& getData() const { return data; }
-
-	//set data
-	void setData(const Byte& data) { this.data = data; }
-};
-
-class W65C02S::WordRegister
-{
-private:
-	//internal data word
-	Word data;
-public:
-	//get data
-	const Word& getWord() const { return data; }
-
-	//set data
-	void setWord(const Word& word) { data = word; }
-
-	//get the high byte of the internal data
-	const Byte& getHighByte();
-
-	//set the high byte of the internal data
-	void setHighByte(const Byte& byte);
-
-	//get the low byte of the internal data
-	const Byte& getLowByte();
-
-	//set the low byte of the internal data
-	void setLowByte(const Byte& byte);
 };
