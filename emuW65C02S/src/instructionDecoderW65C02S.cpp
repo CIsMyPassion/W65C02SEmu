8146acfee50646164850a806aece57cd864c8735
CIsMyPassion <sjno@gmx.de>
2021-02-15 04:00:39 +0100

    brk and ora decoing working and testing

--- emuW65C02S/src/instructionDecoderW65C02S.cpp
+++ emuW65C02S/src/instructionDecoderW65C02S.cpp
@@ -3,7 +3,7 @@
 W65C02S::Instruction W65C02S::InstructionDecoder::getInstruction()
 {
 
-	Byte maskedOpCode = LOWER_HALF_MASK & opCode;
+	Byte maskedOpCode = UPPER_HALF_MASK & opCode;
 
 	if (maskedOpCode == ROW0)
 	{
@@ -39,7 +39,7 @@ W65C02S::Instruction W65C02S::InstructionDecoder::getInstruction()
 	}
 	else
 	{
-		throw 48;
+		throw 1;
 	}
 }
 
@@ -48,10 +48,6 @@ W65C02S::Instruction W65C02S::InstructionDecoder::determineROW0Instruction()
 	if (opCode == (Byte) OpCode::BRK)
 	{
 		return Instruction::BRK;
-	}
-	else if (opCode == (Byte) OpCode::BPL_P)
-	{
-		return Instruction::BPL;
 	}
 	else if (opCode == (Byte) OpCode::ORA_ZII ||
 			opCode == (Byte) OpCode::ORA_ZIIY ||
@@ -65,42 +61,109 @@ W65C02S::Instruction W65C02S::InstructionDecoder::determineROW0Instruction()
 	{
 		return Instruction::ORA;
 	}
-
-	return Instruction::LDA;
+	else if (opCode == (Byte) OpCode::UNDEFINED02 ||
+			opCode == (Byte) OpCode::UNDEFINED03 ||
+			opCode == (Byte) OpCode::UNDEFINED0b ||
+			opCode == (Byte) OpCode::UNDEFINED13 ||
+			opCode == (Byte) OpCode::UNDEFINED1b)
+	{
+		return Instruction::UNDEFINED;
+	}
+	else if (opCode == (Byte) OpCode::TSB_Z ||
+			opCode == (Byte) OpCode::TSB_AI)
+	{
+		return Instruction::TSB;
+	}
+	else if (opCode == (Byte) OpCode::ASL_Z ||
+			opCode == (Byte) OpCode::ASL_ACC ||
+			opCode == (Byte) OpCode::ASL_AI ||
+			opCode == (Byte) OpCode::ASL_ZIX ||
+			opCode == (Byte) OpCode::ASL_AIX)
+	{
+		return Instruction::ASL;
+	}
+	else if (opCode == (Byte) OpCode::RMB0_Z)
+	{
+		return Instruction::RMB0;
+	}
+	else if (opCode == (Byte) OpCode::PHP_S)
+	{
+		return Instruction::PHP;
+	}
+	else if (opCode == (Byte) OpCode::BBR0_P)
+	{
+		return Instruction::BBR0;
+	}
+	else if (opCode == (Byte) OpCode::BPL_P)
+	{
+		return Instruction::BPL;
+	}
+	else if (opCode == (Byte) OpCode::TRB_Z ||
+			opCode == (Byte) OpCode::TRB_AI)
+	{
+		return Instruction::TRB;
+	}
+	else if (opCode == (Byte) OpCode::RMB1_Z)
+	{
+		return Instruction::RMB1;
+	}
+	else if (opCode == (Byte) OpCode::CLC)
+	{
+		return Instruction::CLC;
+	}
+	else if (opCode == (Byte) OpCode::INC_ACC)
+	{
+		return Instruction::INC;
+	}
+	else if (opCode == (Byte) OpCode::BBR1_P)
+	{
+		return Instruction::BBR1;
+	}
+	else
+	{
+		throw 1;
+	}
 }
 
 W65C02S::Instruction W65C02S::InstructionDecoder::determineROW1Instruction()
 {
+	throw 1;
 	return Instruction::LDA;
 }
 
 W65C02S::Instruction W65C02S::InstructionDecoder::determineROW2Instruction()
 {
+	throw 1;
 	return Instruction::LDA;
 }
 
 W65C02S::Instruction W65C02S::InstructionDecoder::determineROW3Instruction()
 {
+	throw 1;
 	return Instruction::LDA;
 }
 
 W65C02S::Instruction W65C02S::InstructionDecoder::determineROW4Instruction()
 {
+	throw 1;
 	return Instruction::LDA;
 }
 
 W65C02S::Instruction W65C02S::InstructionDecoder::determineROW5Instruction()
 {
+	throw 1;
 	return Instruction::LDA;
 }
 
 W65C02S::Instruction W65C02S::InstructionDecoder::determineROW6Instruction()
 {
+	throw 1;
 	return Instruction::LDA;
 }
 
 W65C02S::Instruction W65C02S::InstructionDecoder::determineROW7Instruction()
 {
+	throw 1;
 	return Instruction::LDA;
 }
 
--- testW65C02S/src/tests/W65C02STestInstructionDecoding.cpp
+++ testW65C02S/src/tests/W65C02STestInstructionDecoding.cpp
@@ -18,11 +18,51 @@ public:
 
 };
 
-TEST_F(W65C02STestInstructionDecoding, ROW0Decoding)
+TEST_F(W65C02STestInstructionDecoding, BRK_Decoding)
 {
 	using namespace W65C02S;
 
 	decoder.setOpCode((Byte) OpCode::BRK);
-	Instruction brk = decoder.getInstruction();
-	ASSERT_EQ(brk, Instruction::BRK);
+	ASSERT_EQ(decoder.getInstruction(), Instruction::BRK);
+}
+
+TEST_F(W65C02STestInstructionDecoding, ORA_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::ORA_ZII);
+	Instruction ora_zii = decoder.getInstruction();
+	ASSERT_EQ(ora_zii, Instruction::ORA);
+
+	decoder.setOpCode((Byte) OpCode::ORA_ZIIY);
+	Instruction ora_ziiy = decoder.getInstruction();
+	ASSERT_EQ(ora_ziiy, Instruction::ORA);
+
+	decoder.setOpCode((Byte) OpCode::ORA_ZI);
+	Instruction ora_zi = decoder.getInstruction();
+	ASSERT_EQ(ora_zi, Instruction::ORA);
+
+	decoder.setOpCode((Byte) OpCode::ORA_Z);
+	Instruction ora_z = decoder.getInstruction();
+	ASSERT_EQ(ora_z, Instruction::ORA);
+
+	decoder.setOpCode((Byte) OpCode::ORA_ZIX);
+	Instruction ora_zix = decoder.getInstruction();
+	ASSERT_EQ(ora_zix, Instruction::ORA);
+
+	decoder.setOpCode((Byte) OpCode::ORA_I);
+	Instruction ora_i = decoder.getInstruction();
+	ASSERT_EQ(ora_i, Instruction::ORA);
+
+	decoder.setOpCode((Byte) OpCode::ORA_AIY);
+	Instruction ora_aiy = decoder.getInstruction();
+	ASSERT_EQ(ora_aiy, Instruction::ORA);
+
+	decoder.setOpCode((Byte) OpCode::ORA_AI);
+	Instruction ora_ai = decoder.getInstruction();
+	ASSERT_EQ(ora_ai, Instruction::ORA);
+
+	decoder.setOpCode((Byte) OpCode::ORA_AIX);
+	Instruction ora_aix = decoder.getInstruction();
+	ASSERT_EQ(ora_aix, Instruction::ORA);
 }
