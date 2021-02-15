02eba218ed8715ab8b74bf8236cd5ceb8a8cbe7c
CIsMyPassion <sjno@gmx.de>
2021-02-15 19:36:49 +0100

    added more tests for decoding instructions

--- testW65C02S/src/tests/W65C02STestInstructionDecoding.cpp
+++ testW65C02S/src/tests/W65C02STestInstructionDecoding.cpp
@@ -363,3 +363,255 @@ TEST_F(W65C02STestInstructionDecoding, DEC_Decoding)
 	Instruction dec_acc = decoder.getInstruction();
 	ASSERT_EQ(dec_acc, Instruction::DEC);
 }
+
+TEST_F(W65C02STestInstructionDecoding, RTI_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::RTI_S);
+	Instruction rti_s = decoder.getInstruction();
+	ASSERT_EQ(rti_s, Instruction::RTI);
+}
+
+TEST_F(W65C02STestInstructionDecoding, EOR_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::EOR_ZII);
+	Instruction eor_zii = decoder.getInstruction();
+	ASSERT_EQ(eor_zii, Instruction::EOR);
+
+	decoder.setOpCode((Byte) OpCode::EOR_Z);
+	Instruction eor_z = decoder.getInstruction();
+	ASSERT_EQ(eor_z, Instruction::EOR);
+
+	decoder.setOpCode((Byte) OpCode::EOR_I);
+	Instruction eor_i = decoder.getInstruction();
+	ASSERT_EQ(eor_i, Instruction::EOR);
+
+	decoder.setOpCode((Byte) OpCode::EOR_A);
+	Instruction eor_a = decoder.getInstruction();
+	ASSERT_EQ(eor_a, Instruction::EOR);
+
+	decoder.setOpCode((Byte) OpCode::EOR_ZIIY);
+	Instruction eor_ziiy = decoder.getInstruction();
+	ASSERT_EQ(eor_ziiy, Instruction::EOR);
+
+	decoder.setOpCode((Byte) OpCode::EOR_ZI);
+	Instruction eor_zi = decoder.getInstruction();
+	ASSERT_EQ(eor_zi, Instruction::EOR);
+
+	decoder.setOpCode((Byte) OpCode::EOR_ZIX);
+	Instruction eor_zix = decoder.getInstruction();
+	ASSERT_EQ(eor_zix, Instruction::EOR);
+
+	decoder.setOpCode((Byte) OpCode::EOR_AIY);
+	Instruction eor_aiy = decoder.getInstruction();
+	ASSERT_EQ(eor_aiy, Instruction::EOR);
+
+	decoder.setOpCode((Byte) OpCode::EOR_AIX);
+	Instruction eor_aix = decoder.getInstruction();
+	ASSERT_EQ(eor_aix, Instruction::EOR);
+}
+
+TEST_F(W65C02STestInstructionDecoding, LSR_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::LSR_Z);
+	Instruction lsr_z = decoder.getInstruction();
+	ASSERT_EQ(lsr_z, Instruction::LSR);
+
+	decoder.setOpCode((Byte) OpCode::LSR_ACC);
+	Instruction lsr_acc = decoder.getInstruction();
+	ASSERT_EQ(lsr_acc, Instruction::LSR);
+
+	decoder.setOpCode((Byte) OpCode::LSR_A);
+	Instruction lsr_a = decoder.getInstruction();
+	ASSERT_EQ(lsr_a, Instruction::LSR);
+
+	decoder.setOpCode((Byte) OpCode::LSR_ZIX);
+	Instruction lsr_zix = decoder.getInstruction();
+	ASSERT_EQ(lsr_zix, Instruction::LSR);
+
+	decoder.setOpCode((Byte) OpCode::LSR_AIX);
+	Instruction lsr_aix = decoder.getInstruction();
+	ASSERT_EQ(lsr_aix, Instruction::LSR);
+}
+
+TEST_F(W65C02STestInstructionDecoding, PHA_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::PHA_S);
+	Instruction pha_s = decoder.getInstruction();
+	ASSERT_EQ(pha_s, Instruction::PHA);
+}
+
+TEST_F(W65C02STestInstructionDecoding, JMP_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::JMP_A);
+	Instruction jmp_a = decoder.getInstruction();
+	ASSERT_EQ(jmp_a, Instruction::JMP);
+
+	decoder.setOpCode((Byte) OpCode::JMP_AI);
+	Instruction jmp_ai = decoder.getInstruction();
+	ASSERT_EQ(jmp_ai, Instruction::JMP);
+
+	decoder.setOpCode((Byte) OpCode::JMP_AII);
+	Instruction jmp_aii = decoder.getInstruction();
+	ASSERT_EQ(jmp_aii, Instruction::JMP);
+}
+
+TEST_F(W65C02STestInstructionDecoding, BVC_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::BVC_P);
+	Instruction bvc_p = decoder.getInstruction();
+	ASSERT_EQ(bvc_p, Instruction::BVC);
+}
+
+TEST_F(W65C02STestInstructionDecoding, CLI_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::CLI);
+	Instruction cli = decoder.getInstruction();
+	ASSERT_EQ(cli, Instruction::CLI);
+}
+
+TEST_F(W65C02STestInstructionDecoding, PHY_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::PHY_S);
+	Instruction phy_s = decoder.getInstruction();
+	ASSERT_EQ(phy_s, Instruction::PHY);
+}
+
+TEST_F(W65C02STestInstructionDecoding, RTS_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::RTS_S);
+	Instruction rts_s = decoder.getInstruction();
+	ASSERT_EQ(rts_s, Instruction::RTS);
+}
+
+TEST_F(W65C02STestInstructionDecoding, ADC_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::ADC_ZIIX);
+	Instruction adc_ziix = decoder.getInstruction();
+	ASSERT_EQ(adc_ziix, Instruction::ADC);
+
+	decoder.setOpCode((Byte) OpCode::ADC_Z);
+	Instruction adc_z = decoder.getInstruction();
+	ASSERT_EQ(adc_z, Instruction::ADC);
+
+	decoder.setOpCode((Byte) OpCode::ADC_I);
+	Instruction adc_i = decoder.getInstruction();
+	ASSERT_EQ(adc_i, Instruction::ADC);
+
+	decoder.setOpCode((Byte) OpCode::ADC_A);
+	Instruction adc_a = decoder.getInstruction();
+	ASSERT_EQ(adc_a, Instruction::ADC);
+
+	decoder.setOpCode((Byte) OpCode::ADC_ZIIY);
+	Instruction adc_ziiy = decoder.getInstruction();
+	ASSERT_EQ(adc_ziiy, Instruction::ADC);
+
+	decoder.setOpCode((Byte) OpCode::ADC_ZI);
+	Instruction adc_zi = decoder.getInstruction();
+	ASSERT_EQ(adc_zi, Instruction::ADC);
+
+	decoder.setOpCode((Byte) OpCode::ADC_ZIX);
+	Instruction adc_zix = decoder.getInstruction();
+	ASSERT_EQ(adc_zix, Instruction::ADC);
+
+	decoder.setOpCode((Byte) OpCode::ADC_AIY);
+	Instruction adc_aiy = decoder.getInstruction();
+	ASSERT_EQ(adc_aiy, Instruction::ADC);
+
+	decoder.setOpCode((Byte) OpCode::ADC_AIX);
+	Instruction adc_aix = decoder.getInstruction();
+	ASSERT_EQ(adc_aix, Instruction::ADC);
+}
+
+TEST_F(W65C02STestInstructionDecoding, STZ_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::STZ_Z);
+	Instruction stz_z = decoder.getInstruction();
+	ASSERT_EQ(stz_z, Instruction::STZ);
+
+	decoder.setOpCode((Byte) OpCode::STZ_ZIX);
+	Instruction stz_zix = decoder.getInstruction();
+	ASSERT_EQ(stz_zix, Instruction::STZ);
+}
+
+TEST_F(W65C02STestInstructionDecoding, ROR_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::ROR_Z);
+	Instruction ror_z = decoder.getInstruction();
+	ASSERT_EQ(ror_z, Instruction::ROR);
+
+	decoder.setOpCode((Byte) OpCode::ROR_ACC);
+	Instruction ror_acc = decoder.getInstruction();
+	ASSERT_EQ(ror_acc, Instruction::ROR);
+
+	decoder.setOpCode((Byte) OpCode::ROR_A);
+	Instruction ror_a = decoder.getInstruction();
+	ASSERT_EQ(ror_a, Instruction::ROR);
+
+	decoder.setOpCode((Byte) OpCode::ROR_ZIX);
+	Instruction ror_zix = decoder.getInstruction();
+	ASSERT_EQ(ror_zix, Instruction::ROR);
+
+	decoder.setOpCode((Byte) OpCode::ROR_AIX);
+	Instruction ror_aix = decoder.getInstruction();
+	ASSERT_EQ(ror_aix, Instruction::ROR);
+}
+
+TEST_F(W65C02STestInstructionDecoding, PLA_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::PLA_S);
+	Instruction pla_s = decoder.getInstruction();
+	ASSERT_EQ(pla_s, Instruction::PLA);
+}
+
+TEST_F(W65C02STestInstructionDecoding, BVS_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::BVS_P);
+	Instruction bvs_p = decoder.getInstruction();
+	ASSERT_EQ(bvs_p, Instruction::BVS);
+}
+
+TEST_F(W65C02STestInstructionDecoding, SEI_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::SEI);
+	Instruction sei = decoder.getInstruction();
+	ASSERT_EQ(sei, Instruction::SEI);
+}
+
+TEST_F(W65C02STestInstructionDecoding, PLY_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::PLY_S);
+	Instruction ply_s = decoder.getInstruction();
+	ASSERT_EQ(ply_s, Instruction::PLY);
+}
