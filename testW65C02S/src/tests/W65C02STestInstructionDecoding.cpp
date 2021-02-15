3e501447dbe3bc437a2930003c0cdf337e7e3d66
CIsMyPassion <sjno@gmx.de>
2021-02-15 16:22:11 +0100

    expanding tests

--- testW65C02S/src/tests/W65C02STestInstructionDecoding.cpp
+++ testW65C02S/src/tests/W65C02STestInstructionDecoding.cpp
@@ -66,3 +66,63 @@ TEST_F(W65C02STestInstructionDecoding, ORA_Decoding)
 	Instruction ora_aix = decoder.getInstruction();
 	ASSERT_EQ(ora_aix, Instruction::ORA);
 }
+
+TEST_F(W65C02STestInstructionDecoding, TSB_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::TSB_Z);
+	Instruction tsb_z = decoder.getInstruction();
+	ASSERT_EQ(tsb_z, Instruction::TSB);
+
+	decoder.setOpCode((Byte) OpCode::TSB_AI);
+	Instruction tsb_ai = decoder.getInstruction();
+	ASSERT_EQ(tsb_ai, Instruction::TSB);
+}
+
+TEST_F(W65C02STestInstructionDecoding, ASL_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::ASL_Z);
+	Instruction asl_z = decoder.getInstruction();
+	ASSERT_EQ(asl_z, Instruction::ASL);
+
+	decoder.setOpCode((Byte) OpCode::ASL_ACC);
+	Instruction asl_acc = decoder.getInstruction();
+	ASSERT_EQ(asl_acc, Instruction::ASL);
+	
+	decoder.setOpCode((Byte) OpCode::ASL_AI);
+	Instruction asl_ai = decoder.getInstruction();
+	ASSERT_EQ(asl_ai, Instruction::ASL);
+
+	decoder.setOpCode((Byte) OpCode::ASL_ZIX);
+	Instruction asl_zix = decoder.getInstruction();
+	ASSERT_EQ(asl_zix, Instruction::ASL);
+
+	decoder.setOpCode((Byte) OpCode::ASL_AIX);
+	Instruction asl_aix = decoder.getInstruction();
+	ASSERT_EQ(asl_aix, Instruction::ASL);
+}
+
+TEST_F(W65C02STestInstructionDecoding, RMB_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::RMB0_Z);
+	Instruction rmb0 = decoder.getInstruction();
+	ASSERT_EQ(rmb0, Instruction::RMB0);
+
+	decoder.setOpCode((Byte) OpCode::RMB1_Z);
+	Instruction rmb1 = decoder.getInstruction();
+	ASSERT_EQ(rmb1, Instruction::RMB1);
+}
+
+TEST_F(W65C02STestInstructionDecoding, PHP_Decoding)
+{
+	using namespace W65C02S;
+
+	decoder.setOpCode((Byte) OpCode::PHP_S);
+	Instruction php = decoder.getInstruction();
+	ASSERT_EQ(php, Instruction::PHP);
+}
